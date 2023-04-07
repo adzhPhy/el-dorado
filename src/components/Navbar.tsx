d40ef86e13df14073a03fb63071e95f630c816a2
Ali-Dzhan Ali <adzh.aliali@gmail.com>
2023-04-07 20:08:52 +0300

    Fixed a css error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ import { FiKey } from "react-icons/fi";
 const Navbar = () => (
   <Flex p="2" borderBottom="1px" borderColor="gray.100">
     <Box fontSize="3xl" color="blue.400" fontWeight="bold">
-      <Link href="/" paddingleft="2">
+      <Link href="/" style={{ paddingLeft: "2" }}>
         ElDorado
       </Link>
     </Box>
